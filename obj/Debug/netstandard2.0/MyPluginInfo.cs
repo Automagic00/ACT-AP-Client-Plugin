86869c91eb528b3177fcfbfe2fc9d336e94a9c6b
Devin <superdevin00@gmail.com>
2024-06-13 14:43:59 -0500

    Ignore files generated on build

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ACTAP
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ACTAP";
-        public const string PLUGIN_NAME = "ACTAP";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
